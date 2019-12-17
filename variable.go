4ce584d41d35edd2a2ba73d55f33b48421753b93
Shivank Awasthi <shivank.awasthi@engineerbabu.in>
2019-12-17 23:22:09 +0530

    17/12/19

---
+++ variable.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+
+func main(){
+    var a,b int=1,2
+    var c="string"
+    var e int
+    var bl=true
+    f:="apple"
+    fmt.Println(a,b)
+    fmt.Println(c)
+    fmt.Println(e)
+    fmt.Println(bl)
+    fmt.Println(f)
+
+    
+}
